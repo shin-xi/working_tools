0c73a089462b3f6d6b22505728cc7f26622a8ede
shin <kinda1@qq.com>
2017-12-16 10:01:20 +0800

    commit

---
+++ historyRecord/historyRecord.js
@@ -0,0 +1,43 @@
+var HistoryRecord = /** @class */ (function () {
+    function HistoryRecord(amount) {
+        if (amount === void 0) { amount = 5; }
+        this.amount = amount;
+    }
+    HistoryRecord.prototype.getHis = function () {
+        var invHis = [];
+        for (var i = 0, item = void 0; i < this.amount; i++) {
+            item = localStorage.getItem("history-" + i);
+            if (item !== null) {
+                invHis.push(item);
+            }
+            else if (item === null) {
+                return invHis;
+            }
+        }
+        return invHis;
+    };
+    HistoryRecord.prototype.setHis = function (invHis) {
+        this.removeHisAll();
+        invHis = Array.from(new Set(invHis.reverse())).reverse().filter(function (value) { return value !== undefined && value !== null && value !== ''; });
+        if (invHis.length > 5) {
+            invHis = invHis.slice(-this.amount);
+        }
+        for (var i = 0, item = void 0, len = invHis.length; i < len; i++) {
+            localStorage.setItem("history-" + i, invHis[i]);
+        }
+        return this.getHis();
+    };
+    HistoryRecord.prototype.removeHisSingle = function (invHis, item) {
+        delete invHis[invHis.findIndex(function (value) { return value === item; })];
+        invHis = invHis.filter(function (value) { return value !== undefined; });
+        this.setHis(invHis);
+        return this.getHis();
+    };
+    HistoryRecord.prototype.removeHisAll = function () {
+        for (var i = 0; i < this.amount; i++) {
+            localStorage.removeItem("history-" + i);
+        }
+        return [];
+    };
+    return HistoryRecord;
+}());
---
+++ historyRecord/historyRecord.ts
@@ -0,0 +1,55 @@
+class HistoryRecord {
+
+    constructor(public amount: number = 5) {
+
+    }
+
+    getHis(): any[] {
+        let invHis: any[] = [];
+        for (let i = 0, item; i < this.amount; i++) {
+            item = localStorage.getItem(`history-${i}`);
+
+            if (item !== null) {
+                invHis.push(item);
+            } else if (item === null) {
+                return invHis;
+            }
+        }
+        return invHis;
+    }
+
+    setHis(invHis): any[] {
+
+        this.removeHisAll();
+
+        invHis = Array.from(new Set(invHis.reverse())).reverse().filter((value) => value !== undefined && value !== null && value !== '');
+
+        if (invHis.length > 5) {
+            invHis = invHis.slice(-this.amount);
+        }
+
+        for (let i = 0, item, len = invHis.length; i < len; i++) {
+            localStorage.setItem(`history-${i}`, invHis[i]);
+        }
+
+        return this.getHis();
+    }
+
+    removeHisSingle(invHis, item): any[] {
+
+        delete invHis[invHis.findIndex((value) => value === item)];
+        invHis = invHis.filter((value) => value !== undefined);
+
+        this.setHis(invHis);
+        return this.getHis();
+    }
+
+    removeHisAll(): any[] {
+
+        for (let i = 0; i < this.amount; i++) {
+            localStorage.removeItem(`history-${i}`);
+        }
+        return [];
+    }
+
+}
